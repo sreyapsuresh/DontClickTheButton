8b422ceb1cc8819ecaf72dc83b24204e5d11cc29
sreyapsuresh <sreyapsuresh@gmail.com>
2024-03-20 22:38:09 -0500

    Initial Commit

---
+++ DontClickTheButton/DontClickTheButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontClickTheButtonApp.swift
+//  DontClickTheButton
+//
+//  Created by Sreya Suresh on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct DontClickTheButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
